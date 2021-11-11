7d2e3a6a7d29138286a0cbb55cc991a2778dfae1
FylthyPlayz <jacob_cook_football@yahoo.com>
2021-11-11 14:07:41 -0600

    Footer pieces

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,5 +1,12 @@
+import { footerUsers, showFavoritesBox } from "./nav/Footer.js"
+
 export const GiffyGram = () => {
 
     // Show main main UI
-    return `<h1>Giffygram</h1>`
+    return `<h1>Giffygram</h1>
+    <footer>
+        
+        ${footerUsers()}
+    </footer>
+    `
 }
--- src/scripts/nav/Footer.js
+++ src/scripts/nav/Footer.js
@@ -1,6 +1,22 @@
 import { getUsers } from "../data/provider.js";
 
-export const Users = () => { // this will create the function to represent the posts by users from a dropdown menu of the footer
+export const showFavoritesBox = () => { // this will create the function to represent and show the favorited posts when the user checks the box in the footer
+    const favoriteCheckBox = getFavorites()
+    return `
+    <h3>Show only favorites</h3>
+    <select id="favorite">
+        <option value="0"></option>
+        ${favorites.map(
+        (favoriteObj) => {
+            return `
+                <input type="checkbox" id="check" value="${favoriteObj.id}" /> ${favoriteObj.post}`
+        }
+    )}
+            `
+
+}
+
+export const footerUsers = () => { // this will create the function to represent the posts by users from a dropdown menu of the footer
     const users = getUsers()
     return `
     <h3>Posts by user</h3> 
@@ -16,4 +32,22 @@ export const Users = () => { // this will create the function to represent the p
         }
         </select>
         `
+}
+
+export const selectDates = () => {
+    const postDates = getDates()
+    return `
+    <h3>Posts since</h3>
+    <select id="date">
+        <option value="0"></option>
+        ${dates.map(
+        (dateObj) => {
+            return `
+                <option value="${dateObj.id}">${dateObj.date}</option>
+                `
+        }
+    )
+        }
+        </select>
+    `
 }
