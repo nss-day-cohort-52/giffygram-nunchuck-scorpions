9ddc6c36c7e991be21ea5983d1aff0255650846f
bytesparallel <jason.ross841@gmail.com>
2021-11-12 11:23:36 -0600

    NavBar now appears on page, working on getting links set up

--- src/scripts/nav/NavBar.js
+++ src/scripts/nav/NavBar.js
@@ -1,13 +1,17 @@
 export const NavBar = () => {
     let navbarHTML = ""
 
-    navbarHTML = "<ul>"
+    navbarHTML += "<ul>"
+    
+    navbarHTML +
+
+
 }
 
 export const DmButton = () => {
     let DmBttnHTML = `` 
     
-    html += `<button type="button" onclick= ${};> Direct Message </button>`
+    html += `<button type="button" onclick= somethingplaceholder;> Direct Message </button>`
 
     return DmBttnHTML
 }
